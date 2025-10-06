eff0d17a1150e94388a9228ca41e76b451c4b00c
Erenaz <allenzhang8023@gmail.com>
2025-10-06 00:56:15 -0700

    refactor: rename to detector/ + configs/, update references, untrack build/

---
+++ detector/build/CMakeFiles/flndr_day1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for flndr_day1.
+# This may be replaced when dependencies are built.
---
+++ detector/build/CMakeFiles/flndr_day3.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flndr_day3.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/OpticalInit.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/OpticalInit.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/OpticalPropertiesLoader.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/OpticalPropertiesLoader.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/PhotonBudget.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/PhotonBudget.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/PhotonCountActions.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/PhotonCountActions.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/RootrackerPrimaryGenerator.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/RootrackerPrimaryGenerator.cc.o.d"
+  "CMakeFiles/flndr_day3.dir/src/main_day2.cc.o"
+  "CMakeFiles/flndr_day3.dir/src/main_day2.cc.o.d"
+  "flndr_day3"
+  "flndr_day3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flndr_day3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
